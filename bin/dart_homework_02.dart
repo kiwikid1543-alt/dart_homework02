b08bed62f2596050a3f29856c055cecad4c63c0c
kiwikid1543-alt <kiwikid1543@gmail.com>
2025-12-11 16:38:00 +0900

    feat: 2, 3번 해결

--- bin/dart_homework_02.dart
+++ bin/dart_homework_02.dart
@@ -21,10 +21,49 @@ class StudentScore extends Score {
   }
 }
 
-void main() {
-  StudentScore studentInfo = StudentScore(
-    90,
-    '홍길동',
-  ); // studentInfo 이름과 점수를 가지는 객체 생성
-  studentInfo.showInfo(); // 출력
+void main() async {
+  // StudentScore studentInfo = StudentScore(
+  //   90,
+  //   '홍길동',
+  // ); // studentInfo 이름과 점수를 가지는 객체 생성
+  // studentInfo.showInfo(); // 출력
+
+  // 1. 파일불러오기
+  File txtFile = File('students.txt');
+  String content = await txtFile.readAsString();
+  // 2. 첫번째 줄불러오기
+  List<String> lines = content.split('\n');
+  List<Student> studentList = [];
+  // 3. 이름과 점수 구분짓기
+  for (String nameAndScore in lines) {
+    List<String> nameAndScoreList = nameAndScore.split(',');
+    String name = nameAndScoreList[0];
+    int score = int.tryParse(nameAndScoreList[1]) ?? 0;
+    // print(name);
+    // print(score);
+
+    Student s = Student(name, score);
+    studentList.add(s);
+  }
+  print('어떤 학생의 점수를 확인하시겠습니까?');
+  String userIuput = stdin.readLineSync()!; //사용자의 입력을 받아 문자열로 반환
+  Student? target;
+  for (var student in studentList) {
+    if (student.name == userIuput) {
+      print('이름: ${student.name}, 점수:${student.score}');
+      target =
+          student; // target이 student.name과 비교하는 줄 알아서 student.name이라고 적었다, 하지만 student를 넣어줘야한다.
+      break;
+    }
+  }
+
+  if (target == null) {
+    print('잘못된 학생 이름을 입력하셨습니다. 다시 입력해주세요');
+  }
+}
+
+class Student {
+  String name;
+  int score;
+  Student(this.name, this.score);
 }
